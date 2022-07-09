0cbf41616c2712c6f7e3cb6810d23375cdcf5af0
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2022-07-09 16:34:50 -0300

    Update SQL Banco de dados.sql

--- SQL Banco de dados.sql
+++ SQL Banco de dados.sql
@@ -3,6 +3,8 @@ create database cadastro
 default character set utf8mb4
 default collate utf8mb4_general_ci;
 
+desc- mostra conteúdos da tabela!
+drop database cadastro;   /* Apagando banco de dados*/
 
 /* CRIANDO TABELA */
 id int not null auto_increment, /* O Id irá ser gerado automaticamente*/
@@ -15,4 +17,15 @@ altura float(3,2),
 nacionalidade varchar(20) default ' Brasil', /* Varchar = quantidade máxima de caractere suportado l Default = quando o usuário nao digitar nada o campo sera preenchido como BRASIL*/
 primary key (id)
 )default charset = utf8mb4;
+
+
+/*GERANDO BACKUP*/
+
+clicar em server - data export - dump (escolher o tipo de dump) = escolher como exportar ( arquivo único) = include create schema depois clica em start.
+importar para um novo servidor - data import
+
+update gafanhotos set nome = 'fulano' where id = '5';   /* alterando dado da tabela*/
+
+
+
 
